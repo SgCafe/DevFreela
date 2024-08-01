9aa4c9e2a483abef57af101a8911dcb722007a72
SgCafe <105067158+SgCafe@users.noreply.github.com>
2024-07-31 22:37:04 -0300

    feat: add endpoints

---
+++ DevFreela.API/Controllers/UsersControllers.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Controllers;
+
+    [ApiController]
+    [Route("api/users")]
+public class UsersControllers : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetAllUsers()
+    {
+        return Ok();
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetById(int id)
+    {
+        return Ok();
+    }
+}
