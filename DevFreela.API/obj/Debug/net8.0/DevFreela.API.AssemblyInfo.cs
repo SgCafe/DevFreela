f558bca3b76ded00d00080e8e8b1a2e8f599e920
Sergio-Sum <105067158+SgCafe@users.noreply.github.com>
2024-08-13 22:17:23 -0300

    :sparkles: feat: Configurando o EF Core

--- DevFreela.API/obj/Debug/net8.0/DevFreela.API.AssemblyInfo.cs
+++ DevFreela.API/obj/Debug/net8.0/DevFreela.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DevFreela.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4d18bd7aa7f44f381923295a020f27036b1292c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f81cae60e7bcf8681cb473fd4809d03eec3dddc")]
 [assembly: System.Reflection.AssemblyProductAttribute("DevFreela.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DevFreela.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
